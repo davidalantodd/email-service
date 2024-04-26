ab83f96c7eb1e42b6a3dc61581734356db4a60c3
davidalantodd <david.todd@multiverse.io>
2024-04-26 16:34:52 -0400

    parameterize email, subject, and message in handler

--- handler.js
+++ handler.js
@@ -3,9 +3,24 @@ const AWS = require('aws-sdk');
 const ses = new AWS.SES({ region: 'us-east-2'});
 
 module.exports.sendEmail = async (event) => {
+  const queryParams = event.queryStringParameters || {};
+  let { email, message, subject } = queryParams;
+
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Email is required',
+      }),
+    };
+  }
+
+  message = message || "This is a message generated automatically from a Lambda function!";
+  subject = subject || "Hello from Lambda"
+
   const params = {
     Destination: {
-      ToAddresses: ['davidtodd39@gmail.com']
+      ToAddresses: [email]
     },
     Message: {
       Body: {
@@ -17,21 +32,20 @@ module.exports.sendEmail = async (event) => {
         Data: 'Hello from Lambda',
       },
     },
-    Source: 'davidtodd39@gmail.com'
+    Source: email
   }
+  
   await ses.sendEmail(params).promise();
+  
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `Email sent to ${params.Destination.ToAddresses}`,
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
         input: event,
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
